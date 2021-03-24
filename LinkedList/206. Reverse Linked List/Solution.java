d7e845ff9ce29ed0befe4d4873b0c26db18207e3
SAMRAT MITRA <samratmitra338@gmail.com>
2021-03-24 17:06:28 +0530

    Create Solution.java

---
+++ LinkedList/206. Reverse Linked List/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
+//Author: Samrat Mitra
+//Better than 100% Solutions
