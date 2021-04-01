0500dd2168ee21c9e74365c5dcfe9b1a2c324408
SAMRAT MITRA <samratmitra338@gmail.com>
2021-04-01 13:20:24 +0530

    Create Solution.java

---
+++ String/28. Implement strStr()/Solution.java
@@ -0,0 +1,8 @@
+//Author: Samrat Mitra
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Implement strStr().
+//Memory Usage: 37.5 MB, less than 76.82% of Java online submissions for Implement strStr().
