67b4723f9ee632d072a311d2f99ea701325852cf
SAMRAT MITRA <samratmitra338@gmail.com>
2021-04-16 09:09:59 +0530

    Create Solution.java

---
+++ April Leetcode Challenge/Day-15/509. Fibonacci Number/Solution.java
@@ -0,0 +1,15 @@
+//Dynamic Programming Approach
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i< n + 1; i++){
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
+    }
+}
