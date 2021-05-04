29adc107b582752a3d17dce5b9ee14539fbaae2c
SAMRAT MITRA <samratmitra338@gmail.com>
2021-05-04 09:56:48 +0530

    Create Solution.java

---
+++ May Leetcode Challenge/Day-03/1480. Running Sum of 1d Array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] runningSum(int[] res) {
+    int temp = res[0];
+    
+	if(res.length == 0){
+        return new int[res.length];
+    }
+            
+    for(int i = 1; i < res.length; i++){
+        res[i] = res[i] + temp;
+        temp = res[i];
+    }
+    
+    return res;    
+    }
+}
