40496d6729599ef633b8561e9f7d0626e9e990dd
Samrat Mitra <samratmitra338@gmail.com>
2021-05-31 10:18:48 +0530

    Create Solution.java

---
+++ Matrix/1572. Matrix Diagonal Sum/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        for(int i=0; i<mat.length; i++){
+            sum += mat[i][i] + mat[i][mat.length-1-i];
+            if(i == mat.length - 1 - i){
+                sum -= mat[i][i];
+            }
+        }
+        return sum;
+    }
+}
