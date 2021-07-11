d008ccb0ee904324c81a904e9616028a1cbdd8b5
Samrat Mitra <samratmitra338@gmail.com>
2021-07-11 23:50:31 +0530

    Create Solution.java

---
+++ July LeetCode Challenge/Day-11/Find Median from Data Stream/Solution.java
@@ -0,0 +1,45 @@
+class MedianFinder {
+    List<Integer> arr;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        arr = new ArrayList<>();
+    }
+    
+    public void addNum(int num) {
+        int idx = binarySearch(num);
+        arr.add(idx, num);
+    }
+    
+    private int binarySearch(int k) {
+        int l = 0;
+        int r = arr.size()-1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr.get(mid) == k) {
+                return mid;
+            } else if (arr.get(mid) < k) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+    
+    public double findMedian() {
+        if (arr.size() % 2 != 0) {
+            return arr.get(arr.size()/2);
+        } else {
+            int l = arr.get(arr.size()/2 - 1);
+            int r = arr.get(arr.size()/2);
+            return (l + r) / 2.0;
+        }
+    }
+}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
