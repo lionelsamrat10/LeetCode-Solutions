a9cab70bf363358300fbf7c8f9fc0ee88e0b1fbb
Samrat Mitra <samratmitra338@gmail.com>
2021-07-16 23:53:17 +0530

    Create Solution.java

---
+++ July LeetCode Challenge/Day-16/4Sum/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+  
+    public List<List<Integer>> kSum(int[] nums, int target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (start == nums.length || nums[start] * k > target || target > nums[nums.length - 1] * k)
+            return res;
+        if (k == 2)
+            return twoSum(nums, target, start);
+        for (int i = start; i < nums.length; ++i)
+            if (i == start || nums[i - 1] != nums[i])
+                for (var set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+                    res.get(res.size() - 1).addAll(set);
+                }
+        return res;
+    }
+  
+    public List<List<Integer>> twoSum(int[] nums, int target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Integer> s = new HashSet<>();
+        for (int i = start; i < nums.length; ++i) {
+            if (res.isEmpty() || res.get(res.size() - 1).get(1) != nums[i])
+                if (s.contains(target - nums[i]))
+                    res.add(Arrays.asList(target - nums[i], nums[i]));
+            s.add(nums[i]);
+        }
+        return res;
+    }
+}
+
