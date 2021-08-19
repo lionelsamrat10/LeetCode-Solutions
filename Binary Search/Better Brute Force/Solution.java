c9a8dc21c79d069dbb8664e7c9615cfc2b83a20a
Samrat Mitra <samratmitra338@gmail.com>
2021-08-19 12:51:06 +0530

    Delete Binary Search/Better Brute Force directory

--- Binary Search/Better Brute Force/Solution.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public boolean judgeSquareSum(int c) {
-       for(long a = 0; a * a <= c; a++) {
-           double b = Math.sqrt(c - (int) (a * a));
-           if(b == (int) b) return true;
-       }
-        return false;
-    }
-}
