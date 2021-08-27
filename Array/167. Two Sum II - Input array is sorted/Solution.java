81af053138224069d222643a04f14781b43d41eb
Samrat Mitra <samratmitra338@gmail.com>
2021-08-27 23:46:56 +0530

    Create Solution.java

---
+++ Array/167. Two Sum II - Input array is sorted/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length -1;
+        while(low <= high) {
+            if(numbers[low] + numbers[high] == target) {
+                return new int[] {low + 1, high + 1};
+            } else if(numbers[low] + numbers[high] > target){
+                high--;
+            } else {
+                low++;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
