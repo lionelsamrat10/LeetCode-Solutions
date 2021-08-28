30cac7f23f2ce29136f0978a8e22130ef65a9651
Samrat Mitra <samratmitra338@gmail.com>
2021-08-29 01:12:28 +0530

    Create Solution.java

---
+++ Two Pointer Approach/905. Sort Array By Parity/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while(low < high) {
+            if(nums[low] % 2 > nums[high] % 2){
+                int temp = nums[low];
+                nums[low] = nums[high];
+                nums[high] = temp;
+            }
+            if(isEvenOdd(nums[low]))
+                low++;
+            if(!isEvenOdd(nums[high]))
+                high--;
+        }
+        return nums;
+    }
+    public boolean isEvenOdd(int num) {
+        return num % 2 == 0 ? true : false;
+    }
+}
+
+// O(n) Solution
