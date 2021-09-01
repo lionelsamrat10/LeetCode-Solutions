eeb28cdc31bbe1944c218e290531b4bee4cfc290
Samrat Mitra <samratmitra338@gmail.com>
2021-09-01 23:59:36 +0530

    Create Solution.java

---
+++ Array/169. Majority Element/Solution.java
@@ -0,0 +1,12 @@
+// Using Boyer-Moore Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for(int num : nums) {
+            if(count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
