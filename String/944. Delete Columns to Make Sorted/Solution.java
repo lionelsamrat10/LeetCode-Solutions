68a66fa831f24e6fb93011b7de57998dadce36b4
Samrat Mitra <samratmitra338@gmail.com>
2021-09-12 23:42:19 +0530

    Create Solution.java

---
+++ String/944. Delete Columns to Make Sorted/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int del = 0;
+        for(int i=0; i<strs[0].length(); i++)
+            for(int j = 0; j<strs.length-1; j++)
+                if(strs[j].charAt(i) >strs[j+1].charAt(i)){
+                    del++;
+                    break;
+                }
+        return del;
+    }
+}
