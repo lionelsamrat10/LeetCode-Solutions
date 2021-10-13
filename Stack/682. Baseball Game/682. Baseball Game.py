e959bf73f0218bdc0414b66078bfcd1960ab7c3b
Srikar-ai <70573179+Srikar-ai@users.noreply.github.com>
2021-10-13 10:29:02 +0530

    Create 682. Baseball Game.py
    code added for #51  pls check @lionelsamrat10

---
+++ Stack/682. Baseball Game/682. Baseball Game.py
@@ -0,0 +1,33 @@
+# You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+# At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+# An integer x - Record a new score of x.
+# "+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+# "D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+# "C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+# Return the sum of all the scores on the record.
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack();
+
+        for(String op : ops) {
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newtop = top + stack.peek();
+                stack.push(top);
+                stack.push(newtop);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } else {
+                stack.push(Integer.valueOf(op));
+            }
+        }
+
+        int ans = 0;
+        for(int score : stack) ans += score;
+        return ans;
+    }
+}
