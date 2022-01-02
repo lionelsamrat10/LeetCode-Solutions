e898388f4379c465683c423ab2287b6adf8fbbc5
Samrat Mitra <samratmitra338@gmail.com>
2022-01-03 00:31:28 +0530

    Create Solution.java

---
+++ Tree/543. Diameter of Binary Tree/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] diameter = new int[1];
+        height(root, diameter);
+        return diameter[0];
+    }
+    private int height(TreeNode root, int[] diameter) {
+        if(root == null) return 0;
+        int leftHeight = height(root.left, diameter);
+        int rightHeight = height(root.right, diameter);
+        diameter[0] = Math.max(diameter[0], leftHeight + rightHeight);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
